64ee899a2ca0f1c1011822920117b63026575c25
Aditya J Gupta <adityagupta4213@yandex.com>
2016-12-28 13:57:26 +0530

    Changed right panel grid's numbers' color

--- js/drawGrid.js
+++ js/drawGrid.js
@@ -6,7 +6,7 @@ function drawGrid() {
     var t = '<table cellspacing="0" border="1" bordercolor="black" cellpadding="0" class="grxd">';
     for (var i = 1; i <= (x * y); i++) {
         t += (i == 1 ? '<tr>' : '');
-        t += '<td style="cursor:pointer; text-align:center;">'+i+'</td>';
+        t += '<td style="cursor:pointer; text-align:center; color:#fff;">'+i+'</td>';
         if (i == (x * y)) {
             t += '</tr>';
         } else {
